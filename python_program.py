69484bfd47562f5b2f4df5b4441e80d4abe2162d
Ishini Hathurusinghe <101358580+ishinihathurusinghe@users.noreply.github.com>
2022-08-29 14:53:50 +0530

    Add files via upload

---
+++ python_program.py
@@ -0,0 +1,18 @@
+
+input1 = input("Enter the first number: ")
+print(input1)
+
+input2 = input("Enter the second number: ")
+print(input2)
+
+first_num = int(input1)
+second_num = int(input2)
+
+
+the_sum =first_num + second_num
+the_product = first_num * second_num
+
+#print the sum
+print("Sum:",the_sum)
+#print the product
+print("Product:",the_product)
